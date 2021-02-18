932b6d4153703c6eb9681619eecaa0f96461ee56
Nikita Kiselev <kiselev2008@gmail.com>
2021-02-19 00:21:51 +0300

    Play the ambiance

--- Lamp.ino
+++ Lamp.ino
@@ -5,76 +5,19 @@
 #define VS1053_DCS    21
 #define VS1053_DREQ   17
 
-#include <ESPmDNS.h>
-#include <WiFiUdp.h>
-#include <ArduinoOTA.h>
-
 // Encoder
 #define CLK 25
 #define DT 27
 #define SW 26
-#include "GyverEncoder.h"
-Encoder enc1(CLK, DT, SW);
 
+#include <ESP32Encoder.h>
+ESP32Encoder encoder;
+unsigned long encoder2lastToggled;
 
-
-// ---------- МАТРИЦА ---------
-#define BRIGHTNESS 40         // стандартная маскимальная яркость (0-255)
-#define CURRENT_LIMIT 1000    // лимит по току в миллиамперах, автоматически управляет яркостью (пожалей свой блок питания!) 0 - выключить лимит
-
-#define WIDTH 16              // ширина матрицы
-#define HEIGHT 16             // высота матрицы
-
-#define COLOR_ORDER RGB       // порядок цветов на ленте. Если цвет отображается некорректно - меняйте. Начать можно с RGB
-
-#define MATRIX_TYPE 0         // тип матрицы: 0 - зигзаг, 1 - параллельная
-#define CONNECTION_ANGLE 0    // угол подключения: 0 - левый нижний, 1 - левый верхний, 2 - правый верхний, 3 - правый нижний
-#define STRIP_DIRECTION 1     // направление ленты из угла: 0 - вправо, 1 - вверх, 2 - влево, 3 - вниз
-// при неправильной настройке матрицы вы получите предупреждение "Wrong matrix parameters! Set to default"
-// шпаргалка по настройке матрицы здесь! https://alexgyver.ru/matrix_guide/
-
-// ============= ДЛЯ РАЗРАБОТЧИКОВ =============
-#define LED_PIN 33             // пин ленты
-#define BTN_PIN 26
-#define MODE_AMOUNT 18
-
-#define NUM_LEDS WIDTH * HEIGHT
-#define SEGMENTS 1            // диодов в одном "пикселе" (для создания матрицы из кусков ленты)
-// ---------------- БИБЛИОТЕКИ -----------------
-#define FASTLED_INTERRUPT_RETRY_COUNT 0
-#define FASTLED_ALLOW_INTERRUPTS 0
-#define FASTLED_ESP8266_RAW_PIN_ORDER
-
-#include <FastLED.h>
-
-CRGB leds[NUM_LEDS];
-
-static const byte maxDim = max(WIDTH, HEIGHT);
-struct {
-  byte brightness = 50;
-  byte speed = 30;
-  byte scale = 40;
-} modes[MODE_AMOUNT];
-
-const byte dawnOffsets[] = {5, 10, 15, 20, 25, 30, 40, 50, 60};
-byte dawnMode;
-boolean dawnFlag = false;
-float thisTime;
-boolean manualOff = false;
-boolean sendSettings_flag = false;
-int8_t currentMode = 0;
-boolean loadingFlag = true;
-boolean ONflag = true;
-uint32_t eepromTimer;
-boolean settChanged = false;
-// Конфетти, Огонь, Радуга верт., Радуга гориз., Смена цвета,
-// Безумие 3D, Облака 3D, Лава 3D, Плазма 3D, Радуга 3D,
-// Павлин 3D, Зебра 3D, Лес 3D, Океан 3D,
-unsigned char matrixValue[8][16];
-String lampIP = "";
-byte hrs, mins, secs;
-byte days;
-String timeStr = "00:00";
+#include <AceButton.h>
+using namespace ace_button;
+AceButton button(SW);
+void handleButton(AceButton*, uint8_t, uint8_t);
 
 // Default volume
 unsigned int volume = 80;
@@ -88,123 +31,154 @@ const char *password = "kiselev1";
 
 //  http://icecast.omroep.nl/radio5-bb-mp3
 // http://ambiance-staging.nikitakiselev.ru/storage/music/zonsonderganglite.mp3
-const char *host = "ambiance-staging.nikitakiselev.ru";
+const char *host = "ambiance.nikitakiselev.ru";
 const char *path = "/storage/music/zonsonderganglite.mp3";
 int httpPort = 80;
 
 // The buffer size 64 seems to be optimal. At 32 and 128 the sound might be brassy.
-uint8_t mp3buff[64];
+uint8_t mp3buff[200];
+
+char *tracks[] = {
+  "/storage/music/sowulo-yule.mp3",
+  "/storage/music/dzivia-vataha.mp3",
+  "/storage/music/dzivia-voryva.mp3",
+  "/storage/music/ursprung-gebo.mp3",
+  "/storage/music/sowulo-f230cele.mp3",
+  "/storage/music/sowulo-mabon.mp3",
+  "/storage/music/sowulo-noodlot-proloog.mp3",
+  "/storage/music/sowulo-brego-in-breoste.mp3",
+  "/storage/music/ivar-bj248rnson-einar-selvik-um-heilage-fjell.mp3",
+  "/storage/music/ursprung-hamingja.mp3",
+  "/storage/music/dzivia-dzikaje-palava324nie.mp3",
+};
+byte totalTracks = 11;
+byte trackIndex = 0;
+bool isPlaying = false;
+unsigned long totalBytes = 0;
+bool playNextPressed = false;
 
 void setup() {
-    enc1.setType(TYPE2);
-
-    FastLED.addLeds<WS2812B, LED_PIN, COLOR_ORDER>(leds, NUM_LEDS)/*.setCorrection( TypicalLEDStrip )*/;
-  FastLED.setBrightness(BRIGHTNESS);
-  if (CURRENT_LIMIT > 0) FastLED.setMaxPowerInVoltsAndMilliamps(5, CURRENT_LIMIT);
-  FastLED.show();
-   
-    Serial.begin(115200);
-
-    // Wait for VS1053 and PAM8403 to power up
-    // otherwise the system might not start up correctly
-    delay(3000);
-
-    // This can be set in the IDE no need for ext library
-    // system_update_cpu_freq(160);
-
-    Serial.println("\n\nSimple Radio Node WiFi Radio");
-
-    SPI.begin();
-
-    player.begin();
-    player.switchToMp3Mode();
-    player.setVolume(volume);
-
-    Serial.print("Connecting to SSID ");
-    Serial.println(ssid);
-    WiFi.begin(ssid, password);
-
-    while (WiFi.status() != WL_CONNECTED) {
-        delay(500);
-        Serial.print(".");
-    }
+  pinMode(SW, INPUT_PULLUP);
+  button.setEventHandler(handleButton);
+  
+  encoder.attachHalfQuad(DT, CLK);
+  encoder.setCount(volume);
 
 
-    ArduinoOTA
-    .onStart([]() {
-      String type;
-      if (ArduinoOTA.getCommand() == U_FLASH)
-        type = "sketch";
-      else // U_SPIFFS
-        type = "filesystem";
 
-      // NOTE: if updating SPIFFS this would be the place to unmount SPIFFS using SPIFFS.end()
-      Serial.println("Start updating " + type);
-    })
-    .onEnd([]() {
-      Serial.println("\nEnd");
-    })
-    .onProgress([](unsigned int progress, unsigned int total) {
-      Serial.printf("Progress: %u%%\r", (progress / (total / 100)));
-    })
-    .onError([](ota_error_t error) {
-      Serial.printf("Error[%u]: ", error);
-      if (error == OTA_AUTH_ERROR) Serial.println("Auth Failed");
-      else if (error == OTA_BEGIN_ERROR) Serial.println("Begin Failed");
-      else if (error == OTA_CONNECT_ERROR) Serial.println("Connect Failed");
-      else if (error == OTA_RECEIVE_ERROR) Serial.println("Receive Failed");
-      else if (error == OTA_END_ERROR) Serial.println("End Failed");
-    });
+  Serial.begin(115200);
 
-  ArduinoOTA.begin();
+  // Wait for VS1053 and PAM8403 to power up
+  // otherwise the system might not start up correctly
+  delay(3000);
 
-    Serial.println("WiFi connected");
-    Serial.println("IP address: ");
-    Serial.println(WiFi.localIP());
+  // This can be set in the IDE no need for ext library
+  // system_update_cpu_freq(160);
 
-    Serial.print("connecting to ");
-    Serial.println(host);
+  Serial.println("\n\nSimple Radio Node WiFi Radio");
 
-    if (!client.connect(host, httpPort)) {
-        Serial.println("Connection failed");
-        return;
-    }
+  SPI.begin();
 
-    Serial.print("Requesting stream: ");
-    Serial.println(path);
+  player.begin();
+  player.switchToMp3Mode();
+  player.setVolume(volume);
 
-    client.print(String("GET ") + path + " HTTP/1.1\r\n" +
-                 "Host: " + host + "\r\n" +
-                 "Connection: close\r\n\r\n");
+  Serial.print("Connecting to SSID ");
+  Serial.println(ssid);
+  WiFi.begin(ssid, password);
+
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(500);
+    Serial.print(".");
+  }
+
+  Serial.println("WiFi connected");
+  Serial.println("IP address: ");
+  Serial.println(WiFi.localIP());
 }
 
 void loop() {
-  enc1.tick();
-  if (enc1.isRight()) {
-    volume = volume + 1;
-    player.setVolume(volume);
-  }
-  if (enc1.isLeft()) {
-    volume = volume - 1;
-    player.setVolume(volume);
-  }
-  
-    if (!client.connected()) {
-        Serial.println("Reconnecting...");
-        if (client.connect(host, httpPort)) {
-            client.print(String("GET ") + path + " HTTP/1.1\r\n" +
-                         "Host: " + host + "\r\n" +
-                         "Connection: close\r\n\r\n");
-        }
+  button.check();
+  if (volume != encoder.getCount()) {
+
+    if (encoder.getCount() > 100) {
+      encoder.setCount(100);  
     }
 
+    if (encoder.getCount() < 0) {
+      encoder.setCount(0);  
+    }
+    
+    volume = encoder.getCount();
+    player.setVolume(volume);
+    Serial.print("New Volume: ");
+    Serial.println(volume);
+  }
+
+  if (playNextPressed) {
+    playNextPressed = false;
+    client.stop();
+    totalBytes = 0;
+
+    if (trackIndex + 1 > (totalTracks - 1)) {
+        trackIndex = 0;
+      } else {
+        trackIndex++;
+      }
+
+    Serial.println("Play next clicked");
+  }
+
+  if (! client.connected()) {
+      Serial.print("Total Bytes Read: ");
+    Serial.println(totalBytes);
+
+    if (totalBytes > 0) {
+      totalBytes = 0;
+      if (trackIndex + 1 > (totalTracks - 1)) {
+        trackIndex = 0;
+      } else {
+        trackIndex++;
+      }
+
+      Serial.print("Play next index: ");
+      Serial.println(trackIndex);
+    }
+    
+
+    Serial.print("Connecting to ambiance: ");
+    Serial.println(tracks[trackIndex]);
+
+    if (client.connect(host, httpPort)) {
+      client.print(String("GET ") + tracks[trackIndex] + " HTTP/1.1\r\n" +
+                   "Host: " + host + "\r\n" +
+                   "Connection: close\r\n\r\n");
+    }
+
+    totalBytes = 0;
+  } else {
     if (client.available() > 0) {
-        // The buffer size 64 seems to be optimal. At 32 and 128 the sound might be brassy.
-        uint8_t bytesread = client.read(mp3buff, 64);
-        player.playChunk(mp3buff, bytesread);
-    }
 
-   effectsTick();
-   buttonTick();
-   ArduinoOTA.handle();
+      // The buffer size 64 seems to be optimal. At 32 and 128 the sound might be brassy.
+      uint8_t bytesread = client.read(mp3buff, 200);
+
+      // delay(100);
+
+      totalBytes += bytesread;
+      player.playChunk(mp3buff, bytesread);
+    }
+  }
+}
+
+void handleButton(AceButton* /*button*/, uint8_t eventType, uint8_t /*buttonState*/) {
+  switch (eventType) {
+    case AceButton::kEventPressed:
+      // digitalWrite(LED_BUILTIN, LED_ON);
+      break;
+    case AceButton::kEventReleased:
+      
+      Serial.println("Button Released");
+      playNextPressed = true;
+      break;
+  }
 }
